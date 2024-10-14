7ee0a69c7b028a5f352c54312c5b04ce41216b6b
Alice Laila <alice.pires01@fatec.sp.gov.br>
2024-10-14 09:51:33 -0300

    cadastro

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function local(){
-    let dados = [ { "id":1,"nome":"alice","senha":123 }, 
-                  { "id":2,"nome":"gustavo","senha":777 }, 
-                  { "id":3,"nome":"leon","senha":333 } 
+    let dados = [ { "id":1,"nome":"alice","senha":123,"numero":11, "email": "alice123@gmail.com"}, 
+                  { "id":2,"nome":"gustavo","senha":777,"numero":11, "email": "alice123@gmail.com"}, 
+                  { "id":3,"nome":"leon","senha":333, "numero":11, "email": "alice123@gmail.com"} 
                 ]       
     let n = JSON.stringify(dados);
     localStorage.setItem("tds", n);   
@@ -11,7 +11,7 @@ function local(){
 
 function logon(){  
   const dados = JSON.parse(localStorage.getItem("tds"))
-  let login = document.querySelector("#login").value
+  let email = document.querySelector("#email").value
   let senha = document.querySelector("#senha").value
 
 for (let i = 0; dados.length > i; i++) {
@@ -19,7 +19,7 @@ for (let i = 0; dados.length > i; i++) {
      alert("Verificando")
     //  alert("encontrou: " + dados[i].nome + ":" + i)
   } else {
-    if (login == dados[i].nome && senha == dados[i].senha) {
+    if (email == dados[i].email && senha == dados[i].senha) {
       console.log("conectado")
       let n = JSON.stringify(dados[i]);
       sessionStorage.setItem("user", n)
@@ -49,28 +49,33 @@ function logaout(){
 
 function adicionar() {
   var ClienteArray = JSON.parse(localStorage.getItem("tds"))
-  let login = document.querySelector("#login").value
+  let nome = document.querySelector("#nome").value
+  let email = document.querySelector("#email").value
   let senha = document.querySelector("#senha").value
-  let user = { id: Date.now(), nome: login, senha: senha }
+  let numero = document.querySelector("#numero").value
+  let user = { id: Date.now(), nome: nome, email: email, senha: senha, numero: numero}
   ClienteArray.push(user)
   localStorage.setItem("tds", JSON.stringify(ClienteArray))
   alert("Registro adicionado.")
-  document.querySelector("#login").value = ""
+  document.querySelector("#email").value = ""
   document.querySelector("#senha").value = ""
+  document.querySelector("#nome").value = ""
+  document.querySelector("#numero").value = ""
+  window.open("index.html")
 }
 
  function buscar() {
    var dados = JSON.parse(localStorage.getItem("tds"))
-   let login = document.querySelector("#login").value
+   let email = document.querySelector("#email").value
 
    for (let i = 0; dados.length > i; i++) {
-     if (dados[i] == null && dados[i] != login) {
+     if (dados[i] == null && dados[i] != email) {
        alert("Verificando")
      } else { 
-       if (login == dados[i].nome) {
+       if (email == dados[i].nome) {
        //alert("encontrou: " + dados[i].nome + ":" + i)
        document.querySelector("#id").value = dados[i].id
-       document.querySelector("#login").value = dados[i].nome
+       document.querySelector("#email").value = dados[i].email
        document.querySelector("#senha").value = dados[i].senha    
        break
      } 
@@ -91,12 +96,12 @@ function atualizar() {
   var dados = JSON.parse(localStorage.getItem("tds"))
   localStorage.removeItem("tds")
   let id = document.querySelector("#id").value
-  let login = document.querySelector("#login").value
+  let email = document.querySelector("#email").value
   let senha = document.querySelector("#senha").value  
 
   for (let i = 0; dados.length > i; i++) {
   if (id == dados[i].id){
-    let user = { id: id, nome: login, senha: senha }
+    let user = { id: id, nome: email, senha: senha }
     dados[i] = user
     localStorage.setItem("tds", JSON.stringify(dados))
     alert("Atualizado!")
@@ -107,7 +112,7 @@ function atualizar() {
 
  function apagarItemVetor() {
    let id = parseInt(document.querySelector("#id").value)
-   let login = document.querySelector("#login").value
+   let email = document.querySelector("#email").value
    var dados = JSON.parse(localStorage.getItem("tds"))
    localStorage.removeItem("tds")
    
@@ -115,7 +120,7 @@ function atualizar() {
      if (dados[i] == null) {
         alert("Verificando")       
      } else { 
-       if (id == dados[i].id && login == dados[i].nome) {
+       if (id == dados[i].id && email == dados[i].nome) {
        delete dados[i]
        break;
      } 
@@ -130,7 +135,7 @@ function apagaTudo(){
 
 function limpar(){
   document.querySelector("#id").value = ""
-  document.querySelector("#login").value = ""
+  document.querySelector("#email").value = ""
   document.querySelector("#senha").value = "" 
 }
 
